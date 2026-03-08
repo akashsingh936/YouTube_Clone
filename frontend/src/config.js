0436ddaec73627217cc8f8822a0e5ccacea49574
akashsingh936 <akashsingh936097@gmail.com>
2026-03-08 12:45:07 +0530

    fix: updated API URL with .env

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+export const BASE_URL = process.env.REACT_APP_API_URL;
+
+
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: BASE_URL,
     headers: {
         "Content-Type": "application/json",
     },
